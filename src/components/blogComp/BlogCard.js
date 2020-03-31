486e3c18d2824721f7a93c184b0d5a7017e4e108
tommy-js <tylerb814925@hotmail.com>
2020-03-31 14:49:37 -0400

    blog updates

--- src/components/blogComp/BlogCard.js
+++ src/components/blogComp/BlogCard.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./blog.css";
+import heart from "../../images/heart.png";
+import heartfull from "../../images/heartfilled.png";
+import twitter from "../../images/twitter.png";
+import twitterfull from "../../images/twitterfilled.png";
+import fbook from "../../images/fbook.png";
+import fbookfull from "../../images/fbookfilled.png";
+import bookmark from "../../images/bookmark.png";
+import bookmarkfull from "../../images/bookmarkfilled.png";
 
 function BlogCard(props) {
   return (
     <div className="blog_card">
-      <p>{props.title}</p>
+      <div className="blog_image"></div>
+      <div className="blog_info">
+        <p className="blog_title">{props.title}</p>
+        <div className="image_block">
+          <img className="blog_icon" src={heartfull} />
+          <div className="image_backdrop"></div>
+          <img className="blog_icon" src={heart} />
+        </div>
+        <div className="image_block">
+          <img className="blog_icon" src={bookmarkfull} />
+          <div className="image_backdrop"></div>
+          <img className="blog_icon" src={bookmark} />
+        </div>
+        <br />
+        <div className="image_block">
+          <img className="blog_icon" src={twitterfull} />
+          <div className="image_backdrop"></div>
+          <img className="blog_icon" src={twitter} />
+        </div>
+        <div className="image_block">
+          <img className="blog_icon" src={fbookfull} />
+          <div className="image_backdrop"></div>
+          <img className="blog_icon" src={fbook} />
+        </div>
+      </div>
     </div>
   );
 }
