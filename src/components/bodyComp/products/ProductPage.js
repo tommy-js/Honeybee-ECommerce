454c5d881c5ac4798cd8523b585ed82a8b156d32
tommy-js <tylerb814925@hotmail.com>
2020-04-01 00:46:26 -0400

    changes to product page

--- src/components/bodyComp/products/ProductPage.js
+++ src/components/bodyComp/products/ProductPage.js
@@ -4,7 +4,10 @@ import styles from "../styles.module.css";
 function ProductPage() {
   return (
     <div className={styles.product_page}>
-      <p>Macbook</p>
+      <div className={styles.product_info}></div>
+      <div className={styles.product_page_image}></div>
+      <div className={styles.product_page_title}>Macbook</div>
+      <div className={styles.product_page_review}></div>
     </div>
   );
 }
